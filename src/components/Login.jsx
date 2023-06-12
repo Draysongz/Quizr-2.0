3f94f3fab364cf06684897b21b8ee34615e1c5e2
draysongz <dharmiegra4@gmail.com>
2023-06-12 18:46:13 +0100

    added login function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,41 +1,76 @@
-import React from 'react'
-import './login.css'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import './login.css';
+import { useNavigate } from 'react-router-dom';
+import { app } from './firebase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 const Login = () => {
-const navigate=useNavigate()
-  const route= ()=>{
-    navigate('/main')
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = getAuth(app);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log(user);
+      toast.success('Logged in');
+      navigate('/main');
+    } catch (error) {
+      const errorMessage = error.message;
+      toast.error(errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    console.log(email);
+    console.log(password);
+  }, [email, password]);
+
   return (
-    <div className='login'>
-    <div className="left">
+    <div className="login">
+      <div className="left">
         <div className="box">
-        <h1 className="heading">
+          <h1 className="heading">
             Digital <br />
             platform <br />
             for distance <br />
-            <span className='learn'>learning.</span>
-        </h1>
-        <h4 className="caption">
-            You will never know everything <br />
-            But you will know more
-        </h4>
+            <span className="learn">learning.</span>
+          </h1>
+          <h4 className="caption">You will never know everything</h4>
         </div>
-        
-    </div>
+      </div>
 
-    <div className="right">
-    <form  className="loginForm">
-        <h2>Hey there, </h2>
-        <p className='des'>Enter the details below</p>
-        <input type="email" placeholder='Email address' name='email' className='email' />
-        <input type="password" placeholder='password' name='Password' className='pass'/>
-        <button className='loginBtn' onClick={route}>Login</button>
-      </form>
+      <div className="right">
+        <form className="loginForm">
+          <h2>Hey there, </h2>
+          <p className="des">Enter the details below</p>
+          <input type="email" placeholder="Email address" name="email" className="email" onChange={handleEmailChange} />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            className="pass"
+            onChange={handlePasswordChange}
+          />
+          <button className="loginBtn" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKs7HCqmOtR7k44IWx31eIXlTq05k_OXg",
+  authDomain: "lp57-dad59.firebaseapp.com",
+  projectId: "lp57-dad59",
+  storageBucket: "lp57-dad59.appspot.com",
+  messagingSenderId: "838285084778",
+  appId: "1:838285084778:web:8e6af38545bd577bf52213",
+  measurementId: "G-SWND9M9NY3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
