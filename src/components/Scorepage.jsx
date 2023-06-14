5bfcf6f9b51179a2a029b1a9f5501e705cf2f977
draysongz <dharmiegra4@gmail.com>
2023-06-14 19:49:26 +0100

    scoreboard

--- src/components/Scorepage.jsx
+++ src/components/Scorepage.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { app } from './firebase';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { useParams } from 'react-router-dom';
-
+import './score.css'
 
 const Scorepage = () => {
-    const { score } = useParams(); 
-  return (
-    <div>
-       <div>
-      <h2>Score Page</h2>
-      <p>Total Score: {score}</p>
-    </div>
-    </div>
-  )
-}
+  const { score } = useParams();
+  const [user, setUser] = useState(null);
 
-export default Scorepage
+  const auth = getAuth(app);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (loggedInUser) => {
+      if (loggedInUser) {
+        // User is signed in
+        setUser(loggedInUser);
+        console.log(loggedInUser);
+      } else {
+        // User is signed out
+        setUser(null);
+      }
+    });
+
+    // Clean up the event listener on component unmount
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className='score'>
+      <div className='scoreboard'>
+        <h2>{user ? user.email : 'Not logged in'}</h2>
+        <p>Total Score: {score}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Scorepage;
